24dd78956643af4630765ac80ba606ac3544e926
Eryk Puławski <eryk.pulawski@pollub.edu.pl>
2018-01-30 00:26:42 +0100

    first commit

---
+++ src/main/java/com/example/restspringbootangular/RestSpringbootAngularApplication.java
@@ -0,0 +1,12 @@
+package com.example.restspringbootangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSpringbootAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSpringbootAngularApplication.class, args);
+	}
+}
